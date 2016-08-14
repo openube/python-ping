eb2e919e217105abff9757bf6fd618daca057d12
EclectickEclipse <ariana.giroux@gmail.com>
2016-08-13 20:12:11 -0600

    do_one now can send a ping without using MyStats

--- ping.py
+++ ping.py
@@ -238,7 +238,6 @@
     ===========================================================================
 """
 
-# TODO make stats collection optional.
 # TODO Remove any calls to time.sleep, to enable extension into larger framework that aren't multi threaded.
 #=============================================================================#
 import os
@@ -310,8 +309,6 @@ def checksum(source_string):
     return answer
 
 
-# TODO Make call to MyStats optional
-# TODO DONE, do_one Usage!
 def do_one(destIP, hostname, timeout, mySeqNumber, numDataBytes,
            myStats=None, quiet=False, ipv6=False):
     """
@@ -351,8 +348,6 @@ def do_one(destIP, hostname, timeout, mySeqNumber, numDataBytes,
         mySocket.close()
         return delay
 
-    # TODO Make calls to MyStats optional
-    # TODO DONE!
     if myStats is not None:
         myStats.pktsSent += 1
 
@@ -376,8 +371,6 @@ def do_one(destIP, hostname, timeout, mySeqNumber, numDataBytes,
                 dataSize, host_addr, icmpSeqNumber, iphTTL, delay)
             )
 
-        # TODO Make calls to MyStats optional
-        # TODO DONE!
         if myStats is not None:
             myStats.pktsRcvd += 1
             myStats.totTime += delay
@@ -535,7 +528,6 @@ def signal_handler(signum, frame):
     sys.exit(0)
 
 
-# TODO verbose_ping makes use of the myStats object.
 def verbose_ping(hostname, timeout=3000, count=3,
                  numDataBytes=64, path_finder=False, ipv6=False):
     """
@@ -547,9 +539,6 @@ def verbose_ping(hostname, timeout=3000, count=3,
         # Handle Ctrl-Break e.g. under Windows
         signal.signal(signal.SIGBREAK, signal_handler)
 
-    # TODO Should verbose ping retain current myStats functionality?
-    # This is a higher level function than do_one or send/receive_one, so it
-    # could be used by a CLI
     myStats = MyStats()  # Reset the stats
 
     mySeqNumber = 0  # Starting value
@@ -567,7 +556,6 @@ def verbose_ping(hostname, timeout=3000, count=3,
         print()
         return
 
-    # TODO Should verbose ping retain current myStats functionality?
     myStats.thisIP = destIP
 
     for i in range(count):
@@ -582,11 +570,9 @@ def verbose_ping(hostname, timeout=3000, count=3,
         if (MAX_SLEEP > delay):
             time.sleep((MAX_SLEEP - delay)/1000)
 
-    # TODO Should verbose ping retain current myStats functionality?
     dump_stats(myStats)
     # 0 if we receive at least one packet
     # 1 if we don't receive any packets
-    # TODO Should verbose ping retain current myStats functionality?
     return not myStats.pktsRcvd
 
 # =============================================================================#
@@ -597,7 +583,6 @@ def quiet_ping(hostname, timeout=3000, count=3,
     """
     Same as verbose_ping, but the results are returned as tuple
     """
-    # TODO Make call to MyStats optional
     myStats = MyStats()  # Reset the stats
     mySeqNumber = 0 # Starting value
 
@@ -610,14 +595,12 @@ def quiet_ping(hostname, timeout=3000, count=3,
     except socket.gaierror:
         return False
 
-    # TODO Make call to MyStats optional
     myStats.thisIP = destIP
 
     # This will send packet that we don't care about 0.5 seconds before it starts
     # actually pinging. This is needed in big MAN/LAN networks where you sometimes
     # loose the first packet. (while the switches find the way... :/ )
     if path_finder:
-        # TODO Make call to MyStats optional
         fakeStats = MyStats()
         do_one(fakeStats, destIP, hostname, timeout,
                mySeqNumber, numDataBytes, quiet=True, ipv6=ipv6)
@@ -636,16 +619,13 @@ def quiet_ping(hostname, timeout=3000, count=3,
         if (MAX_SLEEP > delay):
             time.sleep((MAX_SLEEP - delay) / 1000)
 
-    # TODO Make call to MyStats optional
     if myStats.pktsSent > 0:
         myStats.fracLoss = (myStats.pktsSent - myStats.pktsRcvd) / myStats.pktsSent
 
-    # TODO Make call to MyStats optional
     if myStats.pktsRcvd > 0:
         myStats.avrgTime = myStats.totTime / myStats.pktsRcvd
 
     # return tuple(max_rtt, min_rtt, avrg_rtt, percent_lost)
-    # TODO Make call to MyStats optional
     return myStats.maxTime, myStats.minTime, myStats.avrgTime, myStats.fracLoss
 
 # =============================================================================#
